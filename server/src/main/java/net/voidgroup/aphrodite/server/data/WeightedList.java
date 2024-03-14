841b5fd4f872e9bdbce49c09454ebd9eb3784165
CelDaemon <celdaemon@voidgroup.net>
2024-03-14 10:29:25 +0100

    Async icon loading, optionals instead of null, and non-spamming unknown icon errors

--- server/src/main/java/net/voidgroup/aphrodite/server/data/WeightedList.java
+++ server/src/main/java/net/voidgroup/aphrodite/server/data/WeightedList.java
@@ -3,17 +3,19 @@ package net.voidgroup.aphrodite.server.data;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.PrimitiveCodec;
 import net.minecraft.util.math.random.Random;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
-public class WeightedList<T>{
+public class WeightedList<T> implements Iterable<WeightedValue<T>> {
     private final List<WeightedValue<T>> values;
+    private final int totalWeight;
     public WeightedList(List<WeightedValue<T>> values) {
         this.values = values;
         this.totalWeight = values.stream().map(WeightedValue::weight).reduce(0, Integer::sum);
     }
-    private final int totalWeight;
     public Optional<T> getRandom(Random random) {
         if(totalWeight < 1) return Optional.empty();
         var rand = random.nextInt(totalWeight);
@@ -30,5 +32,10 @@ public class WeightedList<T>{
                 .xmap(
                         WeightedList::new,
                         (WeightedList<T> obj) -> obj.values);
+    }
+    @NotNull
+    @Override
+    public Iterator<WeightedValue<T>> iterator() {
+        return values.iterator();
     }
 }
